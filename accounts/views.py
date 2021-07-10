291e4add4625ffcf1285803a6e54fc8c4d44755a
nasir733 <engnasir733@gmail.com>
2021-07-10 18:35:59 +0500

    added the login page

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 from django.contrib import messages,auth
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        username= request.POST['username']
+        password=request.POST['password']
+        user =auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request=request,user=user)
+            messages.success(request, 'you are logged in ')
+            return redirect('landingPage')
+        else:
+            messages.error(request, 'invalid credintials')
+            return redirect('login')
     return render(request,'accounts/login.html')
 
 def logout_user(request):
@@ -18,8 +29,20 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
-        user =User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,)
-        user.save()
-        messages.success(request, 'Account created succesfully')
-        return redirect('login')
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'email already exits')
+                else:
+                    user =User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,)
+                    user.save()
+                    messages.success(request, 'Account created succesfully')
+                    return redirect('login')
+
+        else:
+            messages.error(request,'password do not match')
+            return redirect('register')
     return render(request,'accounts/registeration.html')

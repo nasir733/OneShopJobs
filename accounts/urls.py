4f0d1b65363d20c7b3c736adc8e4614fefbadf57
nasir733 <engnasir733@gmail.com>
2021-07-10 17:58:35 +0500

    added basic user registeration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login',views.login,name="login"),
-    path('register',views.register,name="register"),
-    path('logout',views.logout_user,name="logout"),
+    path('login/',views.login,name="login"),
+    path('register/',views.register,name="register"),
+    path('logout/',views.logout_user,name="logout"),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib import messages,auth
 # Create your views here.
 def login(request):
-    pass
+    return render(request,'accounts/login.html')
 
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect("landingPage")
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user =User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,)
+        user.save()
+        messages.success(request, 'Account created succesfully')
+        return redirect('login')
+    return render(request,'accounts/registeration.html')

8cbc04b6b3ad23a2d4ea80517921dff4ed1d9a9d
nasir <engnasir733@gmail.com>
2021-07-22 19:55:36 +0500

    ready to deploy

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -207,5 +207,5 @@ SPECTACULAR_SETTINGS = {
 # DATABASE_ROUTERS = ['database-routers.db_routers.FastApiDbRouter','database-routers.db_routers.DjangoDbRouter']
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 django_heroku.settings(locals())
